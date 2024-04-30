ad8b7e0d42e505901ae57ed63558a47d6b209b1b
Bamiwo Adebayo <bammietop@gmail.com>
2024-04-30 17:26:07 +0100

    NoSQL

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,6 +4,6 @@
 
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
-    school = mongo_collection.find({"topic": topic})
+    school = mongo_collection.find({"topics": topic})
     list_school = list(school)
     return list_school
