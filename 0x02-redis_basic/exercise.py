e29d885e0c10db68dd591b508161cb56b4252241
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-01 22:24:52 +0100

    Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -31,14 +31,8 @@ class Cache:
 
     def get_str(self, key: str) -> Union[str, bytes]:
         """ Converts btye to str """
-        value = self._redis.get(key)
-        return value.decode("utf-8")
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> int:
         """ Convert byte to int """
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode("utf-8"))
-        except Exception:
-            value = 0
-        return value
+        return self.get(key, fn=int)
